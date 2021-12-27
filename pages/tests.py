b13d0c668d9101f70fd9291de2935475fc06afc3
Jacob Crown <jkorona.pl@gmail.com>
2021-12-27 13:16:51 +0100

    add complete test functionality for homapage

--- pages/tests.py
+++ pages/tests.py
@@ -1,17 +1,31 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from .views import HomePageView
 
 
 class HomepageTests(SimpleTestCase):
     
-    def test_home_page_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+    # method ran before each test
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
 
-    def test_home_page_url_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
+    def test_home_page_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
 
     def test_home_page_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    def test_home_page_contains_correct_html(self):
+        self.assertContains(self.response, 'Homepage')
+
+    def test_home_page_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response,
+                               'Hi! This should not be on this page!')
+
+    def test_home_page_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
