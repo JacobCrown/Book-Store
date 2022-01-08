9b852d0690e9793bc237a73e0b524f450bd122e4
Jacob Crown <jkorona.pl@gmail.com>
2022-01-08 23:08:49 +0100

    add review functionality - model, foreign keys, display in templates

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Review
 
+class ReviewsInline(admin.TabularInline):
+    model = Review
 
 class BookAdmin(admin.ModelAdmin):
+    inlines = [
+        ReviewsInline,
+    ]
     list_display = ['title', 'author', 'price']
 
 admin.site.register(Book, BookAdmin)
--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
 import uuid
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.deletion import CASCADE
 from django.urls import reverse
 
 
@@ -18,3 +20,22 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book_detail', args=[str(self.id)])
+
+
+
+class Review(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=CASCADE,
+        related_name='reviews',
+    )
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.review
+
+    
